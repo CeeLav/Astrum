bfbc8ce68f7d42a35772f827a8a136c4693df151
CeeLav <852515440@qq.com>
2025-09-30 16:33:05 +0800

    三张表

--- AstrumProj/Assets/Script/Generated/Table/Tables.cs
+++ AstrumProj/Assets/Script/Generated/Table/Tables.cs
@@ -14,19 +14,25 @@ namespace cfg
 public partial class Tables
 {
     public TbItem TbItem {get; }
+    public Entity.TbAction TbAction {get; }
     public Entity.TbEntityBase TbEntityBase {get; }
+    public Entity.TbEntityModel TbEntityModel {get; }
 
     public Tables(System.Func<string, ByteBuf> loader)
     {
         TbItem = new TbItem(loader("tbitem"));
+        TbAction = new Entity.TbAction(loader("entity_tbaction"));
         TbEntityBase = new Entity.TbEntityBase(loader("entity_tbentitybase"));
+        TbEntityModel = new Entity.TbEntityModel(loader("entity_tbentitymodel"));
         ResolveRef();
     }
     
     private void ResolveRef()
     {
         TbItem.ResolveRef(this);
+        TbAction.ResolveRef(this);
         TbEntityBase.ResolveRef(this);
+        TbEntityModel.ResolveRef(this);
     }
 }
 
