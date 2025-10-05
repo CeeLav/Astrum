fcba0245a68976b85d5ba54f48706ab82f9f089c
CeeLav <852515440@qq.com>
2025-10-05 21:38:46 +0800

    暂存

--- AstrumProj/Assets/Script/AstrumView/EntityViews/UnitView.cs
+++ AstrumProj/Assets/Script/AstrumView/EntityViews/UnitView.cs
@@ -29,240 +29,23 @@ namespace Astrum.View.EntityViews
         {
             _unitType = unitType;
         }
-        
+
+
         protected override void OnInitialize()
         {
-            ASLogger.Instance.Info($"UnitView: 初始化单位视图，ID: {EntityId}，类型: {_unitType}");
-            // 设置单位特定的组件
-            SetupUnitComponents();
+            
         }
-        
+
         protected override void OnUpdateView(float deltaTime)
         {
-            // 更新单位特定的视觉效果
-            UpdateUnitVisuals(deltaTime);
+            
         }
 
         protected override void OnSyncWithEntity(long entityId)
         {
-            // 同步单位特定的数据
-            SyncUnitData(entityId);
-        }
-        
-        /// <summary>
-        /// 设置单位组件
-        /// </summary>
-        private void SetupUnitComponents()
-        {
             
-            
-            ASLogger.Instance.Info($"UnitView: 设置单位组件完成，ID: {EntityId}");
         }
-        
-        /// <summary>
-        /// 更新单位视觉效果
-        /// </summary>
-        /// <param name="deltaTime">帧时�?/param>
-        private void UpdateUnitVisuals(float deltaTime)
-        {
-            // 更新血条位�?
-            UpdateHealthBarPosition();
-            
-            // 更新选择指示�?
-            UpdateSelectionIndicator();
-        }
-        
-        /// <summary>
-        /// 同步单位数据
-        /// </summary>
-        /// <param name="entityId">实体UniqueId</param>
-        private void SyncUnitData(long entityId)
-        {
-            if (entityId <= 0) return;
-            
-            // TODO: 通过逻辑层的Entity Manager获取Component数据
-            // 示例代码：
-            /*
-            // 获取实体的各种Component来同步数据
-            var positionComponent = LogicCore.EntityManager.GetComponent<PositionComponent>(entityId);
-            if (positionComponent != null && _transform != null)
-            {
-                _transform.position = new Vector3(positionComponent.X, positionComponent.Y, positionComponent.Z);
-            }
-            
-            var healthComponent = LogicCore.EntityManager.GetComponent<HealthComponent>(entityId);
-            if (healthComponent != null)
-            {
-                UpdateHealthDisplay(healthComponent.CurrentHealth, healthComponent.MaxHealth);
-            }
-            
-            var movementComponent = LogicCore.EntityManager.GetComponent<MovementComponent>(entityId);
-            if (movementComponent != null)
-            {
-                // 更新移动相关的视觉效果
-                UpdateMovementEffects(movementComponent.Speed, movementComponent.Direction);
-            }
-            */
-            
-            ASLogger.Instance.Debug($"UnitView: 同步单位数据，实体ID: {entityId}");
-        }
-        
-        /// <summary>
-        /// 创建血条UI
-        /// </summary>
-        private void CreateHealthBarUI()
-        {
-            if (_gameObject == null) return;
-            
-            // 创建血条GameObject
-            _healthBarUI = new GameObject("HealthBar");
-            _healthBarUI.transform.SetParent(_gameObject.transform);
-            _healthBarUI.transform.localPosition = new Vector3(0, 2, 0);
-            
-            // 添加血条组件
-            var healthBarComponent = new HealthBarComponent();
-            AddViewComponent(healthBarComponent);
-            
-            ASLogger.Instance.Info($"UnitView: 创建血条UI，ID: {EntityId}");
-        }
-        
-        /// <summary>
-        /// 创建选择指示�?
-        /// </summary>
-        private void CreateSelectionIndicator()
-        {
-            if (_gameObject == null) return;
-            
-            // 创建选择指示器GameObject
-            _selectionIndicator = new GameObject("SelectionIndicator");
-            _selectionIndicator.transform.SetParent(_gameObject.transform);
-            _selectionIndicator.transform.localPosition = new Vector3(0, 0.1f, 0);
-            
-            // 添加选择指示器组件
-            var selectionComponent = new SelectionIndicatorComponent();
-            AddViewComponent(selectionComponent);
-            
-            ASLogger.Instance.Info($"UnitView: 创建选择指示器，ID: {EntityId}");
-        }
-        
-        /// <summary>
-        /// 更新血条位�?
-        /// </summary>
-        private void UpdateHealthBarPosition()
-        {
-            if (_healthBarUI != null && _transform != null)
-            {
-                // 让血条始终面向摄像机
-                Camera mainCamera = Camera.main;
-                if (mainCamera != null)
-                {
-                    _healthBarUI.transform.LookAt(mainCamera.transform);
-                    _healthBarUI.transform.Rotate(0, 180, 0);
-                }
-            }
-        }
-        
-        /// <summary>
-        /// 更新选择指示�?
-        /// </summary>
-        private void UpdateSelectionIndicator()
-        {
-            // 这里可以更新选择指示器的视觉效果
-            // 如旋转、缩放等动画效果
-        }
-        
-        /// <summary>
-        /// 播放动画
-        /// </summary>
-        /// <param name="animName">动画名称</param>
-        public void PlayAnimation(string animName)
-        {
-            if (_animator != null)
-            {
-                _animator.Play(animName);
-                ASLogger.Instance.Info($"UnitView: 播放动画，ID: {EntityId}，动画: {animName}");
-            }
-        }
-        
-        /// <summary>
-        /// 更新血�?
-        /// </summary>
-        /// <param name="health">当前血�?/param>
-        /// <param name="maxHealth">最大血�?/param>
-        public void UpdateHealthBar(float health, float maxHealth)
-        {
-            var healthBarComponent = GetViewComponent<HealthBarComponent>();
-            if (healthBarComponent != null)
-            {
-                healthBarComponent.UpdateHealth(health, maxHealth);
-            }
-        }
-        
-        /// <summary>
-        /// 显示选择指示�?
-        /// </summary>
-        public void ShowSelectionIndicator()
-        {
-            var selectionComponent = GetViewComponent<SelectionIndicatorComponent>();
-            if (selectionComponent != null)
-            {
-                selectionComponent.Show();
-            }
-        }
-        
-        /// <summary>
-        /// 隐藏选择指示�?
-        /// </summary>
-        public void HideSelectionIndicator()
-        {
-            var selectionComponent = GetViewComponent<SelectionIndicatorComponent>();
-            if (selectionComponent != null)
-            {
-                selectionComponent.Hide();
-            }
-        }
-        
-        /// <summary>
-        /// 更新移动
-        /// </summary>
-        /// <param name="position">目标位置</param>
-        /// <param name="rotation">目标旋转</param>
-        public void UpdateMovement(Vector3 position, Quaternion rotation)
-        {
-            if (_transform != null)
-            {
-                _transform.position = position;
-                _transform.rotation = rotation;
-            }
-        }
-        
-        /// <summary>
-        /// 设置单位类型
-        /// </summary>
-        /// <param name="unitType">单位类型</param>
-        public void SetUnitType(string unitType)
-        {
-            _unitType = unitType;
-        }
-        
-        /// <summary>
-        /// 获取单位类型
-        /// </summary>
-        /// <returns>单位类型</returns>
-        public string GetUnitType()
-        {
-            return _unitType;
-        }
-        
-        /// <summary>
-        /// 获取单位状态信�?
-        /// </summary>
-        /// <returns>状态信�?/returns>
-        public string GetUnitStatus()
-        {
-            return $"单位视图: ID={EntityId}, 类型={_unitType}, 位置={GetWorldPosition()}";
-        }
-        
+
         /// <summary>
         /// 获取UnitView需要的视图组件类型列表
         /// </summary>
@@ -271,7 +54,6 @@ namespace Astrum.View.EntityViews
             return new Type[]
             {
                 typeof(ModelViewComponent), // 管理模型表现
-                typeof(HealthBarComponent), // 管理血条表现
                 typeof(TransViewComponent), // 管理位置
                 typeof(AnimationViewComponent) // 管理动画播放
                 // 你可以根据需要添加更多组件类型，如血条、选中指示等
@@ -279,76 +61,5 @@ namespace Astrum.View.EntityViews
         }
     }
     
-    /// <summary>
-    /// 血条组件（简化实现）
-    /// </summary>
-    public class HealthBarComponent : ViewComponent
-    {
-        private float _currentHealth = 100f;
-        private float _maxHealth = 100f;
-        
-        protected override void OnInitialize()
-        {
-            // 初始化血条
-        }
-        
-        protected override void OnUpdate(float deltaTime)
-        {
-            // 更新血条动画
-        }
-        
-        protected override void OnDestroy()
-        {
-            // 清理血条
-        }
-        
-        protected override void OnSyncData(object data)
-        {
-            // 同步血条数据
-        }
-        
-        public void UpdateHealth(float health, float maxHealth)
-        {
-            _currentHealth = health;
-            _maxHealth = maxHealth;
-        }
-    }
-    
-    /// <summary>
-    /// 选择指示器组件（简化实现）
-    /// </summary>
-    public class SelectionIndicatorComponent : ViewComponent
-    {
-        private bool _isVisible = false;
-        
-        protected override void OnInitialize()
-        {
-            // 初始化选择指示器
-        }
-        
-        protected override void OnUpdate(float deltaTime)
-        {
-            // 更新选择指示器动画
-        }
-        
-        protected override void OnDestroy()
-        {
-            // 清理选择指示器
-        }
-        
-        protected override void OnSyncData(object data)
-        {
-            // 同步选择指示器数据
-        }
-        
-        public void Show()
-        {
-            _isVisible = true;
-        }
-        
-        public void Hide()
-        {
-            _isVisible = false;
-        }
-    }
+
 }
