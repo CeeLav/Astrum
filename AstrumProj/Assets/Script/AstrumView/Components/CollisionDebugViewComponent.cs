92433de8f4c4dd4b150b9f8b7247b229a007e0ef
CeeLav <852515440@qq.com>
2025-10-10 21:30:40 +0800

    GIzmos

--- AstrumProj/Assets/Script/AstrumView/Components/CollisionDebugViewComponent.cs
+++ AstrumProj/Assets/Script/AstrumView/Components/CollisionDebugViewComponent.cs
@@ -222,31 +222,5 @@ namespace Astrum.View.Components
             return new Quaternion((float)q.x, (float)q.y, (float)q.z, (float)q.w);
         }
     }
-    
-    /// <summary>
-    /// Gizmos绘制MonoBehaviour
-    /// Unity要求OnDrawGizmos必须在MonoBehaviour中
-    /// </summary>
-    public class CollisionGizmosDrawer : MonoBehaviour
-    {
-        public CollisionDebugViewComponent DebugComponent;
-        
-        private void OnDrawGizmos()
-        {
-            if (DebugComponent == null) return;
-            
-            // 绘制实体碰撞盒
-            DebugComponent.DrawEntityCollisionGizmos();
-            
-            // 绘制攻击碰撞盒
-            DebugComponent.DrawAttackBoxGizmos();
-        }
-        
-        private void OnDrawGizmosSelected()
-        {
-            // 当选中GameObject时也绘制
-            OnDrawGizmos();
-        }
-    }
 }
 
